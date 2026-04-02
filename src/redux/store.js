55eede4e237375350de1faef01d7fdbbceeba89c
ismayilovmurad499-pixel <ismayilovmurad499@gmail.com>
2026-04-02 22:01:46 +0400

    frontend

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,6 @@ import userSlice from "./features/userSlice";
 import orderReducer from '../slices/orderSlice';
 import commissionReducer from '../slices/commissionSlice';
 import languageReducer from '../slices/languageSlice';
-import notificationReducer from '../slices/Notificationslice'; // ✅ YENİ
 
 export const store = configureStore({
   reducer: {
