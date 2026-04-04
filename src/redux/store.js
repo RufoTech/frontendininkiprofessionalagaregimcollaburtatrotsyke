ac05ada4d4743038736958ad30dbeda08f8372f0
ismayilovmurad499-pixel <ismayilovmurad499@gmail.com>
2026-04-04 13:20:24 +0400

    frontend

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,7 @@ import userSlice from "./features/userSlice";
 import orderReducer from '../slices/orderSlice';
 import commissionReducer from '../slices/commissionSlice';
 import languageReducer from '../slices/languageSlice';
-import notificationReducer from '../slices/Notificationslice';
+import notificationReducer from '../slices/notificationSlice';
 import bloggerReducer from '../slices/bloggerSlice';
 
 export const store = configureStore({
